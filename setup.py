44617f647f37a3ebc52862dda1471b71e21b028f
timdiels <timdiels.m@gmail.com>
2017-09-27 11:19:14 +0200

    2.0.1.dev

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import os
 
 setup_args = dict(
-    version='2.0.0',
+    version='2.0.1.dev',
     name='varbio',
     description='Bioinformatics utility library',
     long_description=Path('README.rst').read_text(),
