b3f1074589ace1fd506c2b9e2108d73217d87b00
Tim Diels <tidie@psb.vib-ugent.be>
2020-10-24 17:19:02 +0200

    Package for conda instead of pypi

--- setup.py
+++ setup.py
@@ -6,22 +6,8 @@ import os
 setup_args = dict(
     version='3.0.0.dev',
     name='varbio',
-    description='Helper library with common code of coexpnetviz and morph',
     long_description=Path('README.rst').read_text(),
-    url='https://gitlab.psb.ugent.be/deep_genome/varbio',
-    author='Tim Diels',
-    author_email='tim@diels.me',
-    license='LGPL3',
     packages=find_packages(),
-    install_requires=[
-        'attrs',
-        'chardet',
-        'humanize',
-        'numpy>=1',
-        'pandas',
-        'pyyaml',
-        'scipy',
-    ],
     extras_require={
         'dev': [
             'numpydoc',
@@ -34,36 +20,6 @@ setup_args = dict(
             'sphinx-rtd-theme',
         ],
     },
-    # https://pypi.python.org/pypi?%3Aaction=list_classifiers
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
-        'Operating System :: POSIX',
-        'Operating System :: POSIX :: AIX',
-        'Operating System :: POSIX :: BSD',
-        'Operating System :: POSIX :: BSD :: BSD/OS',
-        'Operating System :: POSIX :: BSD :: FreeBSD',
-        'Operating System :: POSIX :: BSD :: NetBSD',
-        'Operating System :: POSIX :: BSD :: OpenBSD',
-        'Operating System :: POSIX :: GNU Hurd',
-        'Operating System :: POSIX :: HP-UX',
-        'Operating System :: POSIX :: IRIX',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: POSIX :: Other',
-        'Operating System :: POSIX :: SCO',
-        'Operating System :: POSIX :: SunOS/Solaris',
-        'Operating System :: Unix',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3 :: Only',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Bio-Informatics',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules'
-    ],
 )
 
 # Generate package data
